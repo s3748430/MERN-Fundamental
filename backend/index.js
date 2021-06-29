f03320d55a9ccd743299dc040a47f2f94fd81a0d
s3748430 <62051704+s3748430@users.noreply.github.com>
2021-06-30 00:52:34 +0700

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
 
 const app = express();
 
 const PORT = 3000;
+
+//mongo connection
+
+mongoose.Promise = global.Promise
+
+mongoose.connect('mongodb://localhost/MERNDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//body parser set up
+
+app.use(bodyParser.urlencoded({ extended : true}));
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) =>
     res.send(`Our new MERN application is running ${PORT}`)
@@ -10,4 +28,5 @@ app.get('/', (req, res) =>
 
 app.listen(PORT, () => 
     console.log(`Your MERN server is running on port ${PORT}`)
-)
+);
+
